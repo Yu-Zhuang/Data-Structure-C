571c8b80057f9a9a4564462b46c09613f5658e18
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-26 23:13:24 +0800

    quick sort

--- 4_1_QuickSort.c
+++ 4_1_QuickSort.c
@@ -1,38 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void QuickSort(int *nums, int left, int right); //small to big
-
+//define size of nums
+#define SIZE 7
+//funciton
+void QUICK_SORT(int *nums, int left, int right);
+void PRINT_NUMS(int *nums, int numsSize) { for(int i=0;i<numsSize;i++) printf("%d ", nums[i]); printf("\n");}
+void SWAP(int *a, int *b) { a[0]=b[0]+a[0]-(b[0]=a[0]); }
+// MAIN
 int main(void){
-	int list[7]={3,5,1,2,8,7,4};
-	//print origin
-	for(int i=0;i<7;i++) printf("%d ", list[i]); printf("\n");
-	// quick sort
-	QuickSort(list, 0, 6);
-	// print after sort
-	for(int i=0;i<7;i++) printf("%d ", list[i]); printf("\n");
-	//end
+	int list[SIZE]={3,7,1,8,4,2,6};
+
+	PRINT_NUMS(list,SIZE); 
+	QUICK_SORT(list,0,SIZE);
+	PRINT_NUMS(list,SIZE);
+
 	return 0;
 }
-void QuickSort(int *nums, int left, int right){
-	if(left>=right) return;
-	printf("===========QuickSort===================\n");
-	int base=left, i=left, j=right+1;
+//quick sort function
+void QUICK_SORT(int *nums, int left, int right){
+	int base=left, i=left, j=right;
+	if(i>=j) return;
 	while(i<j){
-		for(int a=0;a<7;a++) printf("%d ", nums[a]); printf("\n"); //(show list[] status)
-		do i++; while(nums[i]<=nums[base]);
+		if(nums[i]>nums[base]&&nums[j]<nums[base]) SWAP(&nums[i], &nums[j]);
+		do i++; while(nums[i]<nums[base]);
 		do j--; while(nums[j]>nums[base]);
-		printf("i:%d j:%d\n", i, j); //(show i,j status)
-		if(nums[i]>nums[base] && nums[j]<nums[base] && i<j){
-			nums[i]^=nums[j]; nums[j]^=nums[i]; nums[i]^=nums[j];
-		}
 	}
-	if(nums[base] != nums[j]){
-		nums[base]^=nums[j]; nums[j]^=nums[base]; nums[base]^=nums[j];
-	}
-	QuickSort(nums, left, j-1);
-	QuickSort(nums, j+1, right);
-	printf("===========END QuickSort===================\n");
+	SWAP(&nums[base], &nums[j]);
+	QUICK_SORT(nums, left, j-1);
+	QUICK_SORT(nums, j+1, right);
 }
-
-
