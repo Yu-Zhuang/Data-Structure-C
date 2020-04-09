ec5a60524ce3caf5eecfd54eee620947801361a9
mild-guy <106711129@gms.tcu.edu.tw>
2020-04-09 23:45:29 +0800

    update

--- 10_Multi_linked_list/main.cpp
+++ 10_Multi_linked_list/main.cpp
@@ -7,12 +7,12 @@
 #define ROUTE_NUM 5
 enum color{
    RED, YELLOW, BLUE, GREEN, BROWN
-};
+}; 
 typedef struct route{
    int name;
-   int color;
+   int color[ROUTE_NUM];
    struct route *B, *F;
-   struct route *W, *S;
+   struct route *S, *N;
 }NODE;
 typedef struct map{
    NODE *red;
@@ -29,108 +29,178 @@ typedef struct table{
 // ***** FUNCTION ANNOUNCEMENT *****
 void FILE_GET(char *file, int *input, int *size);
 
-void IOTA(char *string, int *interget);
-
-void TABLE_INSERT(TABLE *list, int element, int color, int *total, MAP *map);
 void TABLE_INIT(TABLE *list, int size);
-void TABLE_PRINT(TABLE *list, int total);
+void TABLE_PRINT(TABLE *list, int size);
 
-NODE* MAP_INSERT(MAP *map, int name, int color, NODE *site);
+void ROUTE_INSERT(TABLE *DB, int *route, int routeSize, int color, MAP *map);
+NODE* ROUTE_SELETION(MAP *map, int color);
+void ROUTE_PRINT(NODE *route);
 // ***** MAIN *****
 int main(void){
-   // varialbe announcement
+   // VARIABLE announcement
    char file[ROUTE_NUM][6]={"1.cpp", "2.cpp", "3.cpp", "4.cpp", "5.cpp"};
    int input[ROUTE_NUM][MAX_SIZE], size[ROUTE_NUM]={0}, total=0;
    TABLE DB[MAX_SIZE];
-   TABLE_INIT(DB, MAX_SIZE);
    MAP map;
-   map.red=(NODE*)malloc(sizeof(NODE)); map.red->F=NULL;
-   map.yellow=(NODE*)malloc(sizeof(NODE)); map.yellow->F=NULL;
-   map.blue=(NODE*)malloc(sizeof(NODE)); map.blue->F=NULL;
-   map.green=(NODE*)malloc(sizeof(NODE)); map.green->F=NULL;
-   map.brown=(NODE*)malloc(sizeof(NODE)); map.brown->F=NULL;
-   // CONPITION
-   // get file
-   for(int i=0;i<ROUTE_NUM;i++) FILE_GET(file[i],input[i], &size[i]);
-   // put file in to table and add to map
-   for(int i=0;i<ROUTE_NUM;i++)
-      for(int j=0;j<size[i];j++) TABLE_INSERT(DB, input[i][j], i, &total, &map);
-   //print table
-   TABLE_PRINT(DB, total);
-   // print map
-   //MAP_PRINT(&map);
-   //END
+   
+   // INITIALIZATION
+   map.red=(NODE*)malloc(sizeof(NODE)); map.red->name=-1; map.red->F=NULL;
+   map.yellow=(NODE*)malloc(sizeof(NODE)); map.yellow->name=-1; map.yellow->F=NULL;
+   map.blue=(NODE*)malloc(sizeof(NODE)); map.blue->name=-1; map.blue->F=NULL;
+   map.green=(NODE*)malloc(sizeof(NODE)); map.green->name=-1; map.green->F=NULL;
+   map.brown=(NODE*)malloc(sizeof(NODE)); map.brown->name=-1; map.brown->F=NULL;
+   TABLE_INIT(DB, MAX_SIZE);
+   
+   // COMPUTION
+      //get file
+   for(int i=0;i<ROUTE_NUM;i++) 
+      FILE_GET(file[i],input[i], &size[i]);
+      //put data to DB and map
+   for(int i=0;i<ROUTE_NUM;i++) 
+      ROUTE_INSERT(DB, input[i], size[i], i, &map);
+      //print map
+
+   //OUTPUT
+   printf("\n\t=====[ RED LINE ]=====\n");
+   ROUTE_PRINT(map.red);
+   ROUTE_PRINT(DB[8].site);
+   printf("\n\t=====[ YELLOW LINE ]=====\n");
+   ROUTE_PRINT(DB[29].site);
+   ROUTE_PRINT(map.yellow);
+   printf("\n\t=====[ BLUE LINE ]=====\n");
+   ROUTE_PRINT(map.blue);
+   printf("\n\t=====[ GREEN LINE ]=====\n");
+   ROUTE_PRINT(map.green);
+   ROUTE_PRINT(DB[77].site);
+   printf("\n\t=====[ BROWN LINE ]=====\n");
+   ROUTE_PRINT(map.brown);
+   // END
    return 0;
 }
+
 // ***** FUNCTION *****
-NODE* MAP_INSERT(MAP *map, int name, int color, NODE *site){
-   NODE *ptr, *pre;
+void ROUTE_INSERT(TABLE *DB, int *route, int routeSize, int color, MAP *map){
+   int flag=0; // flag==1: start, flag==0: end;
+   NODE *pStart=ROUTE_SELETION(map,color);
+   NODE *preNode=pStart;
+   int preNum=route[0];
+   for(int i=0;i<routeSize;i++){
+      if(route[i]==0 && flag==0) { flag=1; continue; }//start
+      else if(route[i]==0 && flag==1) { flag=0; continue; } //end
+      // new element
+      if(DB[route[i]].name==-1){
+         // create newnode
+         NODE *newnode = (NODE*)malloc(sizeof(NODE));
+         newnode->F=NULL; newnode->B=NULL; newnode->N=NULL; newnode->S=NULL;
+         newnode->name=route[i]; newnode->color[0]=color;
+         // put into DB
+         DB[route[i]].name=route[i]; DB[route[i]].color[0]=color; DB[route[i]].site=newnode;
+         // put into map
+            //if its MAIN route
+         if(flag){
+               // if pedigree is NULL and 
+            if( ! pStart->F) pStart->F=newnode;
+            else{
+                  //if preNode is on MAIN
+               if(preNode->color[0]==color){
+                  preNode->F=newnode;
+                  newnode->B=preNode;
+
+               }  //if preNode is sub
+               else{
+                  preNode->N=newnode;
+                  newnode->B=preNode;
+               }
+            }
+            //record pre element
+            preNode=newnode;
+            preNum=route[i];
+         }//if its sub route
+         else{
+            // and its head of sub route
+            if( ! route[i-1]){
+               preNode=newnode;
+               preNum=route[i];              
+            }else{
+               preNode->F=newnode;
+               newnode->B=preNode;
+            }
+            //record pre element
+            preNode=newnode;
+            preNum=route[i];
+         }
+      }
+      // if element already exist
+      else{
+         //if its MAIN
+         if(flag){
+            // if preNode is MAIN element
+            if(preNode->color[0]==color){
+               preNode->F=DB[route[i]].site;
+               DB[route[i]].site->S=preNode;             
+            }
+            else{
+               preNode->N=DB[route[i]].site;
+               DB[route[i]].site->S=preNode;               
+            }
+         }
+         //if its sub route
+         else{
+            preNode->F=DB[route[i]].site;
+            DB[route[i]].site->S=preNode;          
+         }
+         //record preNode
+         preNode=DB[route[i]].site;
+         preNum=route[i];   
+      }
+   }
+}
+void ROUTE_PRINT(NODE *route){
+   if(route->name==-1) route=route->F;
+   NODE *preNode;
+   int color = route->color[0];
+   printf("START |-");
+   while(route){
+      printf("[%d", route->name); 
+      if(route->N) printf("(N:%d)", route->N->name);
+      if(route->S) printf("(S:%d)", route->S->name);
+      printf("]-");
+      
+      if(color == route->color[0])
+         { preNode=route; route=route->F; }
+      else
+         { preNode=route; route=route->N; }
+   }
+   printf("| END\n");
+}
+NODE* ROUTE_SELETION(MAP *map, int color){
+   NODE *ret;
    switch(color){
-      case 0: ptr = map->red; break;
-      case 1: ptr = map->yellow; break;
-      case 2: ptr = map->blue; break;
-      case 3: ptr = map->green; break;
-      case 4: ptr = map->brown; break;
+      case 0: ret=map->red; break;
+      case 1: ret=map->yellow; break;
+      case 2: ret=map->blue; break;
+      case 3: ret=map->green; break;
+      case 4: ret=map->brown; break;
       default: break;
    }
-   if(site==NULL){
-      NODE *newnode = (NODE*)malloc(sizeof(NODE));
-      newnode->name=name; newnode->color=color;
-      newnode->W=NULL; newnode->S=NULL; newnode->F=NULL; newnode->B=NULL;
-      //if pedigree is empty
-      //else
-      int deri=0;
-      while(ptr){
-         pre=ptr;
-         ptr=ptr->F; 
-         deri=0;
-         if(ptr!=NULL && ptr->color!=color) { ptr = pre->W; deri=1; }
-      }
-      if(deri == 0){
-         newnode->F = pre->F;
-         pre->F = newnode;
-         newnode->B = pre;
-         return newnode;          
-      }
-      else if(deri==1){
-         newnode->F = pre->W;
-         pre->W = newnode;
-         newnode->B = pre;
-         return newnode; 
-      }
-   }
-   else{
-      int flag=0;
-      while(ptr!=NULL) { 
-         pre=ptr; 
-         ptr=ptr->F; 
-         if(ptr!=NULL && ptr->color!=color) { ptr = pre->W; flag=1; }
-      }
-      pre->F = site;
-      site->S = pre;  
-   }
-
-   return site;
+   return ret;
 }
-// TABLE
-void TABLE_INSERT(TABLE *list, int element, int clr, int *total, MAP *map){
-   if(list[element].name == -1){
-      list[element].name = element;
-      list[element].color[0] = clr;
-      list[element].site = MAP_INSERT(map, element, clr, NULL);
-      total[0]+=1;
-   }else{
-      list[element].name = element;
-      list[element].color[1] = clr;
-      MAP_INSERT(map, element, clr, list[element].site);
-   }
+void FILE_GET(char *file, int *input, int *size){
+   FILE *fptr = fopen(file, "r");
+   char ch, str[MAX_SIZE][MAX_ID_SIZE];
+   int j=0, i=0, flag=0;
+   if(fptr){
+      while( (ch=getc(fptr))!= EOF ){
+         while( ch>='0'&&ch<='9'&&ch!=EOF) { str[j][i]=ch; i++; ch=getc(fptr); flag=1; }
+         if(flag) { str[j][i]='\0'; sscanf(str[j],"%d",&input[j]); j++; flag=0; i=0; }
+      }
+   }else printf("\n\tfaild to open the file!\n");
+   size[0]=j;
+   fclose(fptr);
 }
-//  table_print
-void TABLE_PRINT(TABLE *list, int total){
-   for(int i=0;i<total;i++){
-      if(list[i].name == -1) { printf("%d.\t{ (empty) }\n", i); total++; }
-      else if(list[i].color[1]!=-1) printf("%d.\t{ name:%d, color:%d %d,\tsite:%p }\n", i, list[i].name, list[i].color[0], list[i].color[1], list[i].site);
-      else printf("%d.\t{ name:%d, color:%d,\tsite:%p }\n", i, list[i].name, list[i].color[0], list[i].site);
+void TABLE_PRINT(TABLE *list, int size){
+   for(int i=0;i<size;i++){
+      printf("\n");
    }
 }
 void TABLE_INIT(TABLE *list, int size){
@@ -139,27 +209,3 @@ void TABLE_INIT(TABLE *list, int size){
       for(int j=0;j<ROUTE_NUM;j++) list[i].color[j] = -1;
    }
 }
-// FILE
-void FILE_GET(char *file, int *input, int *size){
-   FILE *fptr = fopen(file, "r");
-   char ch, str[MAX_SIZE][MAX_ID_SIZE];
-   int j=0, i=0, flag=0;
-   if(fptr){
-      while( (ch=getc(fptr))!= EOF ){
-         while( ch>='0'&&ch<='9'&&ch!=EOF) { str[j][i]=ch; i++; ch=getc(fptr); flag=1; }
-         if(flag) { str[j][i]='\0'; IOTA(str[j], &input[j]); j++; flag=0; i=0; }
-      }
-   }else printf("\n\tfaild to open the file!\n");
-   size[0]=j;
-   fclose(fptr);
-}
-// string to interger
-void IOTA(char *string, int *interget){
-   int str_len = strlen(string);
-   int ten=1, i=0;
-   interget[0] = 0;
-   for(int i=1;i<str_len;i++) ten*=10;
-   while(ten) { interget[0]+=ten*(string[i]-'0'); i++; ten/=10; }  
-}
-
-
