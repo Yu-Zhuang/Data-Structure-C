4b31eb6c32abb435810c1fe82781a38dd6749992
mild-guy <106711129@gms.tcu.edu.tw>
2020-04-11 14:16:20 +0800

    update: finished, use bsf to search shortest route between a and b station

--- 10_Multi_linked_list/main.cpp
+++ 10_Multi_linked_list/main.cpp
@@ -26,15 +26,28 @@ typedef struct table{
    int color[ROUTE_NUM];
    NODE *site;
 }TABLE; 
+typedef struct q{
+   int val;
+   int fold;
+   struct q *next;
+}QUEUE;
 // ***** FUNCTION ANNOUNCEMENT *****
 void FILE_GET(char *file, int *input, int *size);
 
 void TABLE_INIT(TABLE *list, int size);
 void TABLE_PRINT(TABLE *list, int size);
 
+void MAP_INIT(MAP *map);
+
 void ROUTE_INSERT(TABLE *DB, int *route, int routeSize, int color, MAP *map);
 NODE* ROUTE_SELETION(MAP *map, int color);
 void ROUTE_PRINT(NODE *route);
+int ROUTE_FIND(TABLE *DB, int start, int end);
+
+void ENQUEUE(QUEUE *head, int element);
+int DEQUEUE(QUEUE *head);
+void QUEUE_PRINT(QUEUE *head);
+void CTABLE_PRINT(int *table);
 // ***** MAIN *****
 int main(void){
    // VARIABLE announcement
@@ -44,11 +57,7 @@ int main(void){
    MAP map;
    
    // INITIALIZATION
-   map.red=(NODE*)malloc(sizeof(NODE)); map.red->name=-1; map.red->F=NULL;
-   map.yellow=(NODE*)malloc(sizeof(NODE)); map.yellow->name=-1; map.yellow->F=NULL;
-   map.blue=(NODE*)malloc(sizeof(NODE)); map.blue->name=-1; map.blue->F=NULL;
-   map.green=(NODE*)malloc(sizeof(NODE)); map.green->name=-1; map.green->F=NULL;
-   map.brown=(NODE*)malloc(sizeof(NODE)); map.brown->name=-1; map.brown->F=NULL;
+   MAP_INIT(&map);
    TABLE_INIT(DB, MAX_SIZE);
    
    // COMPUTION
@@ -61,24 +70,115 @@ int main(void){
       //print map
 
    //OUTPUT
-   printf("\n\t=====[ RED LINE ]=====\n");
-   ROUTE_PRINT(map.red);
-   ROUTE_PRINT(DB[8].site);
-   printf("\n\t=====[ YELLOW LINE ]=====\n");
-   ROUTE_PRINT(DB[29].site);
-   ROUTE_PRINT(map.yellow);
-   printf("\n\t=====[ BLUE LINE ]=====\n");
-   ROUTE_PRINT(map.blue);
-   printf("\n\t=====[ GREEN LINE ]=====\n");
-   ROUTE_PRINT(map.green);
-   ROUTE_PRINT(DB[77].site);
-   printf("\n\t=====[ BROWN LINE ]=====\n");
-   ROUTE_PRINT(map.brown);
+   int flag=1;
+   while(flag){
+      printf("\t@主選單@\n(1)印出資料庫與地圖\n(2)查詢車站\n(3)查詢兩站距離\n(0)結束\n請輸入: "); scanf("%d", &flag);
+      if(flag==0) break;
+      if(flag==1){
+         TABLE_PRINT(DB, MAX_SIZE);
+
+         printf("\n\t=====[ RED LINE ]=====\n");
+         ROUTE_PRINT(map.red);
+         ROUTE_PRINT(DB[8].site);
+         printf("\n\t=====[ YELLOW LINE ]=====\n");
+         ROUTE_PRINT(DB[29].site);
+         ROUTE_PRINT(map.yellow);
+         printf("\n\t=====[ BLUE LINE ]=====\n");
+         ROUTE_PRINT(map.blue);
+         printf("\n\t=====[ GREEN LINE ]=====\n");
+         ROUTE_PRINT(map.green);
+         ROUTE_PRINT(DB[77].site);
+         printf("\n\t=====[ BROWN LINE ]=====\n");
+         ROUTE_PRINT(map.brown);        
+      }else if(flag==2){
+         int target=-1;
+         printf("欲查詢車站: "); scanf("%d", &target);
+         if(DB[target].name==-1) printf("查無此站!\n");
+         else{
+            if(DB[target].color[1]==-1) printf("車站顏色: %d\n", DB[target].color[0]);
+            else printf("車站顏色: %d, %d\n", DB[target].color[0], DB[target].color[1]);            
+         }
+      }else if(flag==3){
+         int start=-1, end=-1;
+         printf("起點站: "); scanf("%d", &start);
+         printf("終點站: "); scanf("%d", &end);
+         if(DB[start].name==-1||DB[end].name==-1) printf("輸入錯誤!\n");
+         else{
+            printf("\n*************************\n*\t總共要搭: %d 站\t*\n", ROUTE_FIND(DB,start,end));
+            printf("*************************\n");
+         }
+      }
+   }
    // END
    return 0;
 }
 
 // ***** FUNCTION *****
+int ROUTE_FIND(TABLE *DB,int start, int end){
+   printf("\n========== 搜尋起止站距離 ==========\n");
+   if(start==end) return 0;
+   int ret=0, fd=0;
+   int table[MAX_SIZE]={0};
+   int fold[MAX_SIZE]={0}; 
+   QUEUE *Q=(QUEUE*)malloc(sizeof(QUEUE));
+   NODE *pre, *pos=DB[start].site, *target=DB[end].site;
+
+   // BSF找最短路徑
+   do{       
+      //該站未被拜訪
+      if(table[pos->name]==0) table[pos->name]=1;
+      //該站已被拜訪
+      else { pos=DB[DEQUEUE(Q)].site; continue; }
+      //find
+      if(pos==target) break; 
+      
+      //找附近車站 並放入Q
+      printf("搜尋站: [[%d]]\n", pos->name); //
+      printf("已搜尋站: "); CTABLE_PRINT(table); //
+      //計算第幾層
+      if((pos->B&&fold[pos->B->name]==0) || (pos->F&&fold[pos->F->name]==0) || (pos->S&&fold[pos->S->name]==0) || (pos->N&&fold[pos->N->name]==0)){
+         fd=fold[pos->name]+1;
+      }
+      //
+      if(pos->B) { fold[pos->B->name]=fd;  if(pos->B==target) {ret+=1; break;}  ENQUEUE(Q,pos->B->name); }    
+      if(pos->F) { fold[pos->F->name]=fd; if(pos->F==target) {ret+=1; break;} ENQUEUE(Q,pos->F->name); }
+      if(pos->S) { fold[pos->S->name]=fd; if(pos->S==target) {ret+=1; break;} ENQUEUE(Q,pos->S->name); }
+      if(pos->N) { fold[pos->N->name]=fd; if(pos->N==target) {ret+=1; break;} ENQUEUE(Q,pos->N->name); }         
+      
+      printf("QUEUE: "); QUEUE_PRINT(Q); //
+      
+      //找下一車站;
+      pos=DB[DEQUEUE(Q)].site;
+
+   }while(1);
+
+   printf("\n========== END_搜尋起止站距離 ==========\n");
+   return fd;
+}
+void QUEUE_PRINT(QUEUE *head){
+   head=head->next;
+   while(head){
+      printf("[%d]-", head->val);
+      head=head->next;
+   }printf("\n\n");
+}
+void ENQUEUE(QUEUE *head, int element){
+   QUEUE *newnode=(QUEUE*)malloc(sizeof(QUEUE));
+   newnode->val=element; newnode->next=NULL;
+   while(head->next) head=head->next;
+   head->next=newnode;
+}
+int DEQUEUE(QUEUE *head){
+   int ret=head->next->val;
+   head->next= head->next->next;
+   return ret;
+}
+void CTABLE_PRINT(int *table){
+   printf("|");
+   for(int i=0;i<MAX_SIZE;i++){
+      if(table[i]) printf("%d, ", i);
+   }printf("|\n");
+}
 void ROUTE_INSERT(TABLE *DB, int *route, int routeSize, int color, MAP *map){
    int flag=0; // flag==1: start, flag==0: end;
    NODE *pStart=ROUTE_SELETION(map,color);
@@ -141,13 +241,17 @@ void ROUTE_INSERT(TABLE *DB, int *route, int routeSize, int color, MAP *map){
             }
             else{
                preNode->N=DB[route[i]].site;
-               DB[route[i]].site->S=preNode;               
+               DB[route[i]].site->S=preNode;         
             }
+            DB[route[i]].color[1]=color;
+            DB[route[i]].site->color[1]=color;   
          }
          //if its sub route
          else{
             preNode->F=DB[route[i]].site;
-            DB[route[i]].site->S=preNode;          
+            DB[route[i]].site->S=preNode;
+            DB[route[i]].color[1]=color;
+            DB[route[i]].site->color[1]=color;  
          }
          //record preNode
          preNode=DB[route[i]].site;
@@ -199,9 +303,20 @@ void FILE_GET(char *file, int *input, int *size){
    fclose(fptr);
 }
 void TABLE_PRINT(TABLE *list, int size){
+   printf("[NAME]\t[COLOR]\t[SITE]\n");
    for(int i=0;i<size;i++){
-      printf("\n");
+      if(list[i].name!=-1){
+         if(list[i].color[1]==-1) printf("%d\t%d\t%p\n", list[i].name, list[i].color[0], list[i].site);
+         else printf("%d\t(%d,%d)\t%p\n", list[i].name, list[i].color[0], list[i].color[1], list[i].site);
+      }
    }
+}
+void MAP_INIT(MAP *map){
+   map->red=(NODE*)malloc(sizeof(NODE)); map->red->name=-1; map->red->F=NULL;
+   map->yellow=(NODE*)malloc(sizeof(NODE)); map->yellow->name=-1; map->yellow->F=NULL;
+   map->blue=(NODE*)malloc(sizeof(NODE)); map->blue->name=-1; map->blue->F=NULL;
+   map->green=(NODE*)malloc(sizeof(NODE)); map->green->name=-1; map->green->F=NULL;
+   map->brown=(NODE*)malloc(sizeof(NODE)); map->brown->name=-1; map->brown->F=NULL;   
 }
 void TABLE_INIT(TABLE *list, int size){
    for(int i=0;i<size;i++){
