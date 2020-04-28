8518344cd241d457ab4b3fd4d13562204a5adee7
mild-guy <106711129@gms.tcu.edu.tw>
2020-04-28 16:35:17 +0800

    update

--- 12_1_stack_backtrack_graph/1.cpp
+++ 12_1_stack_backtrack_graph/1.cpp
@@ -3,18 +3,17 @@
 #include <time.h>
 #include <string.h>
 /*
-* 矩陣 最大 20*20
 * 每個邊權重範圍: 1~10
-* 規定 不能有值(名稱)為 0 的點
+* 規定 不能有名稱為 0 的點
 * 函數: 找出 起點 至 終點 最短路徑 與其權重和
 */
-#define MAX 7 //資料為 幾乘幾 的矩陣
-#define MAX_WEIGHT 7*10 //最大權重為10 再乘以 矩陣行列數
+#define MAX 21 //資料為 幾乘幾 的矩陣
+#define MAX_WEIGHT 21*10 //最大權重為10 再乘以 矩陣行列數
 typedef struct s{ //stack資料定義
 	int val;
 	struct s *next;
 }STACK;
-// function annoucement
+// FUNCTION_ANNOUNCEMENT
 	//檔案讀取與創造 的函數
 void FILE_GENERATOR(char *file);
 void FILE_GET(char *file, int input[][MAX]);
@@ -29,49 +28,51 @@ void PATH_PRINT(STACK *s);
 void STACK_PRINT(STACK *s);
 // MAIN
 int main(void){
-	char file[]="test.txt";
+	char file[]="map.txt";
 	int input[MAX][MAX]={0}; //存檔案的 矩陣(map) 
 	int path[MAX]={0}, pathSize=0, pathWeight=0; //存最短路徑 與 權重
 	int start = 0, end = 0; //存 起始 點
 	//FILE_GENERATOR(file);
-// 讀檔, 顯示讀檔結果
+	
+ // 讀檔, 顯示讀檔結果
 	FILE_GET(file, input);
 	MAP_PRINT(input); 
-// 由鍵盤讀入 起點 終點
+ // 由鍵盤讀入 起點 終點
 	printf("請依序輸入 起點 終點: "); scanf("%d %d", &start, &end);
-// FIND()運算 並判斷 是否有找到, 再輸出結果
+ // FIND()運算 -> 判斷 是否有找到 -> 輸出結果
 	if( FIND(input, start, end, path, &pathSize, &pathWeight) ){
-		printf("#最短路徑: ");
+		printf("執行結果:\n\t# 最短路徑: ");
 		for(int i=pathSize-1;i>=0;i--){
-			if(i!=0) printf("%d->", path[i]);
+			if(i!=0) printf("%d -> ", path[i]);
 			else printf("%d\n", path[i]);
 		}
-		printf("#該路徑權重: %d\n", pathWeight);			
+		printf("\t# 該路徑權重: %d\n", pathWeight);			
 	}
-	else printf("#沒有路徑 從%d到%d\n", start, end);
-// END
+	else printf("\t# 沒有路徑 從%d到%d\n", start, end);
+	
+ // END
 	return 0;
 }
 // FUNCTION
 bool FIND(int map[][MAX], int start, int end, int *path, int *pathSize,int *pathWeight){
-	STACK *stack = (STACK*)malloc(sizeof(STACK));
+	STACK *stack = (STACK*)malloc(sizeof(STACK)); // stack
 	stack->val = 0; stack->next = NULL;
-	int check[MAX]={0};
-	int *min_path=NULL, minpath_Size=0, min_weight=MAX_WEIGHT;
-	int flag=0, find=0;
+	int check[MAX]={0}; // check array
+	int *min_path=NULL, minpath_Size=0, min_weight=MAX_WEIGHT; // min_path
+	int flag=0, find=0; // flag:紀錄該路是否已到終點; find:紀錄是否能從start到end
 
 	STACK_PUSH(stack, start);
-	while(stack->next){
+	while(stack->next){ // stack is empty or not?
 		if(stack->next->val == end){ //找到 end 
-			find = 1; // 該圖start可到end 
+			find = 1; // record: 該圖start可到end 
 			int *tmp_path, tmp_weight=0, tmp_pathSize=0;
-			// record Npath and Nweight;
+			// record: Npath and Nweight;
 			tmp_path = PATH_RECORD(stack, map, &tmp_weight, &tmp_pathSize);
 			printf("===================================================\n=\t\t[ FIND ! ]\n=\t*找到的路徑: ");
 			for(int i=tmp_pathSize-1;i>=0;i--) printf("%d >> ", tmp_path[i]); printf("\n");
 			printf("=\t*該路徑權重: %d\n", tmp_weight);
 			printf("===================================================\n\n");
-			// 找 較短路徑
+			// 是否為 較短路徑
 			if(min_weight > tmp_weight){
 				int *tmp = min_path;
 				min_path = tmp_path;
@@ -82,7 +83,9 @@ bool FIND(int map[][MAX], int start, int end, int *path, int *pathSize,int *path
 			// 回分支點
 			while(stack->next && stack->next->val >= 0) check[STACK_POP(stack)]=0; 
 			// 如果走完
-			if(! stack->next) { for(int i=0;i<minpath_Size;i++) path[i]=min_path[i]; pathSize[0]=minpath_Size; pathWeight[0]=min_weight; return find; }
+			if(! stack->next)
+				{ for(int i=0;i<minpath_Size;i++) path[i]=min_path[i]; pathSize[0]=minpath_Size; pathWeight[0]=min_weight; return find; }
+			// 將stack.top 改為 造訪中狀態
 			stack->next->val *= -1;
 		}
 		else{ // 未找到 end
@@ -95,15 +98,15 @@ bool FIND(int map[][MAX], int start, int end, int *path, int *pathSize,int *path
 					flag = 1;				
 				}
 			}
-			if(flag){ //該路未到終點
-				stack->next->val *= -1;
-			}else{ //到該路終點, 回到分支點
+			if(!flag){ //該路已到終點, 回到分支點
 				while(stack->next && stack->next->val >= 0 ) check[STACK_POP(stack)]=0;
 				// 如果走完
-				if(! stack->next) { for(int i=0;i<minpath_Size;i++) path[i]=min_path[i]; pathSize[0]=minpath_Size; pathWeight[0]=min_weight; return find; }
-				stack->next->val *= -1;			
+				if(! stack->next)
+					{ for(int i=0;i<minpath_Size;i++) path[i]=min_path[i]; pathSize[0]=minpath_Size; pathWeight[0]=min_weight; return find; }		
 			}
-			STACK *tmp = stack;
+			stack->next->val *= -1;	// 將stack.top 改為 造訪中狀態
+			STACK *tmp = stack; 
+			// 將造訪中的點 紀錄至 check[] 
 			while(tmp->next) { if(tmp->next->val>=0) check[tmp->next->val]=1; tmp = tmp->next; }
 		}
 		printf("STACK狀態: "); STACK_PRINT(stack); 
@@ -125,7 +128,6 @@ int* PATH_RECORD(STACK *s, int map[][MAX], int *weight, int *pathSize){
 	}
 	for(int i=0;i<size-1;i++)
 		w+=map[path[i]][path[i+1]];
-	
 	weight[0] = w;
 	pathSize[0] = size;
 	return path;
@@ -145,20 +147,21 @@ int STACK_POP(STACK *s){
 		free(tmp);
 		return ret;
 	}
-	else return 0;
+	return 0;
 }
 // PRINT
 void PATH_PRINT(STACK *s){
-	if(!s->next) return;
+	if(!s || !s->next) return;
 	int path[MAX+1]={-1}, count=0;
 	while(s->next){ 
 		if(s->next->val>0){ path[count]=s->next->val; count++; } 
 		s=s->next; 
 	}
 	for(int i=count-1; i>=0; i--) printf("(%d)->", path[i]);
-	printf("|\n");
+	printf("|END\n");
 }
 void STACK_PRINT(STACK *s){
+	if(!s || !s->next) return;
 	while(s->next){
 		printf("[%d]_", s->next->val);
 		s = s->next;
