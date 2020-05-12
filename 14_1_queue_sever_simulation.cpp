e57cb1a90a1bf2e3b5b80507fa35a50954a43fe6
mild-guy <106711129@gms.tcu.edu.tw>
2020-05-12 13:39:58 +0800

    update

--- 14_1_queue_sever_simulation.cpp
+++ 14_1_queue_sever_simulation.cpp
@@ -12,7 +12,7 @@
 # define MAX 20
 
 typedef struct m{
-	char id[20];
+	char id[MAX];
 	int status;
 	int susTimes;
 }MESSAGES;
@@ -27,15 +27,17 @@ typedef struct qh{
 }Q_HEAD;
 
 // ===== FUNCTION_ANNOUNCEMENT =====
-void DRIVER(int run, int*newMessageNum, int*processorRate, float susccessRate);
+void CONDITION_SET(int* run, int *newMessageNum, int *processorRate, float *failedRate);
+void CONDITION_PRINT(int run, int *newMessageNum, int *processorRate, float failedRate);
 
+void DRIVER(int run, int*newMessageNum, int*processorRate, float failedRate);
 void NEW_MESSAGE(Q_HEAD *q_head, int *newMessageNum);
 void PROCESSOR(Q_HEAD *q, int *processorRate, int *totalQueue,int *totalArrived,\
-				int *firstTimes,int *secondTimes, int *totalMessageSent, int *n);
+				int *firstTimes,int *secondTimes, int *totalMessageSent, int *n,\
+				float failedRate, int *qMAX);
 void SEVER_FREE(Q_HEAD *q, int n);
-
 void STATICS(int totalArrived, int totalMessageSent, int totalRunTime,\
-			 int totalQueue, int firstTimes, int secondTimes);
+			 int totalQueue, int firstTimes, int secondTimes, int qMAX);
 
 void EN_QUEUE(Q_HEAD *q, MESSAGES *element);
 MESSAGES* DE_QUEUE(Q_HEAD *q);
@@ -43,64 +45,95 @@ MESSAGES* DE_QUEUE(Q_HEAD *q);
 // ===== MAIN =====
 int main(void){
 	srand(time(NULL));
-	int run = 100,\
+	int chose = 1,\
+		run = 100,\
 		newMessageNum[2] = {0,60},\
 		processorRate[2] = {20,60};
-	float susccessRate = 0.3;
-	DRIVER(run, newMessageNum, processorRate, susccessRate);
+	float failedRate = 0.3;
+
+	while(chose){
+		CONDITION_SET( &run, newMessageNum, processorRate, &failedRate);
+		DRIVER(run, newMessageNum, processorRate, failedRate);	
+		CONDITION_PRINT( run, newMessageNum, processorRate, failedRate);	
+		printf("繼續按1, 結束按0: "); scanf("%d", &chose); fflush(stdin);
+	}
+	printf("\t [ 程式結束 ]\n");
 	return 0;
 }
 
 // ===== FUNCTION =====
-void DRIVER(int run, int*newMessageNum, int*processorRate, float susccessRate){
+void DRIVER(int run, int*newMessageNum, int*processorRate, float failedRate){
 	int totalArrived = 0,\
 		totalMessageSent = 0,\
 		totalRunTime = 0,\
 		totalQueue = 0,\
 		firstTimes = 0,\
 		secondTimes = 0,\
-		n = 0;
+		n = 0,\
+		qMAX = 0;
 	// create queue
 	Q_HEAD *q_head = (Q_HEAD*)malloc(sizeof(Q_HEAD));
 	q_head->count = 0; q_head->next = NULL; q_head->rear = NULL;
 	// loop (timeNotEnd or queueNotEmpty) // 1 run == 1 minutes
 	for(int i=0; i<run OR q_head->next ; i++){
+		printf("\n\t  ---------- | Iteration %d | ----------\n", i+1);
 		if(i<run)
 			NEW_MESSAGE(q_head, newMessageNum);
 		PROCESSOR(q_head, processorRate, &totalQueue, &totalArrived, &firstTimes,\
-					&secondTimes, &totalMessageSent, &n);
+					&secondTimes, &totalMessageSent, &n, failedRate, &qMAX);
 		SEVER_FREE(q_head, n);
 		totalRunTime += 1;
 	}
 	// printStatics
 	STATICS(totalArrived, totalMessageSent, totalRunTime,\
-			totalQueue, firstTimes, secondTimes);
+			totalQueue, firstTimes, secondTimes, qMAX);
+}
+
+void NEW_MESSAGE(Q_HEAD *q_head, int *newMessageNum){
+	char id_templet[]="123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int it_len = strlen(id_templet);
+	// n = rand(0,60)
+	int n = rand()%(newMessageNum[1]-newMessageNum[0]+1) + newMessageNum[0];
+	// create n*newMessage
+	for(int i=0;i<n;i++){
+		MESSAGES *newMessage = (MESSAGES*)malloc(sizeof(MESSAGES));
+		// initialize newMessage
+		for(int i=0;i<14;i++)
+			newMessage->id[i] = id_templet[rand()%it_len]; 
+		newMessage->status = 0; 
+		newMessage->susTimes = 0;
+		// enqueue newMessage
+		EN_QUEUE(q_head, newMessage);
+	}
+	// print the number of messages that arrive
+	printf("# NEW_MESSAGE()-> Number of messages that arrive: %d\n", n);
 }
 
 void PROCESSOR(Q_HEAD *q, int *processorRate, int *totalQueue,int *totalArrived,\
-				int *firstTimes,int *secondTimes, int *totalMessageSent, int *n){
+				int *firstTimes,int *secondTimes, int *totalMessageSent, int *n,\
+				float failedRate, int *qMAX){
+	// record max size of queue
+	if(q->count > qMAX[0]) qMAX[0] = q->count;
 	// qTotalCount += queueCount
 	totalQueue[0] += q->count;
-	// n = rand(20,60) // 產生處理信件數
+	// n = rand(start,end) // 產生處理信件數
 	n[0] = rand()%(processorRate[1]-processorRate[0]+1) + processorRate[0];
 	// if(n > queueCount) : n = queueCount
-	if(n[0] > q->count)
-		n[0] = q->count;
+	if(n[0] > q->count) n[0] = q->count;
 	// loop(n) // 一封一封寄
 	NODE *qTmp = q->next;
-	printf("\n[n]======[ ID ]==============[status | susTimes]\n");
+	printf("# PROCESSOR()->  (Current Queue size: %d )\n\t[n]======[ ID ]==============[ status | susTimes ]\n", q->count);
 	for(int i=0; i<n[0] AND qTmp ; i++){
-		// suscessful rate = 70%
-		qTmp->val->status = ( (rand()%10)>2 ) ? 1 : 0;
-		printf("hello world\n");
+		// suscessful rate 
+		qTmp->val->status = ( (rand()%10)>(failedRate*10-1) ) ? 1 : 0;
 		qTmp->val->susTimes += 1;
 		// print the ID of the run
-		printf("%d.\t %s \t %d\t %d\n", i+1, qTmp->val->id, qTmp->val->status, qTmp->val->susTimes);
-		// if(send) :
+		printf("\t%d.\t %s \t %d\t %d\n", i+1, qTmp->val->id, qTmp->val->status, qTmp->val->susTimes);
+		// if(send suscessful) :
 		if(qTmp->val->status){
 			// totalArrived += 1;
 			totalArrived[0] += 1;
-			// if(susTimes==1) 1stTimes++; if(susTimes==2) 2ndTimes++;
+			// 1stTimes++, 2ndTimes calculate;
 			switch(qTmp->val->susTimes){
 				case 1: firstTimes[0]++; break;
 				case 2: secondTimes[0]++; break;
@@ -126,56 +159,30 @@ void SEVER_FREE(Q_HEAD *q, int n){
 		qTmp = qTmp->next;
 	}
 	// print the number of messages that are sent
-	printf("[ Number of messages that are sent successfully ]: %d\n", sentNum);
-}
-void NEW_MESSAGE(Q_HEAD *q_head, int *newMessageNum){
-	char id_templet[]="123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int it_len = strlen(id_templet);
-	// n = rand(0,60)
-	int n = rand()%(newMessageNum[1]-newMessageNum[0]+1) + newMessageNum[0];
-	// create n*newMessage
-	for(int i=0;i<n;i++){
-		MESSAGES *newMessage = (MESSAGES*)malloc(sizeof(MESSAGES));
-		// initialize newMessage
-		for(int i=0;i<14;i++)
-			newMessage->id[i] = id_templet[rand()%it_len]; 
-		newMessage->status = 0; 
-		newMessage->susTimes = 0;
-		// enqueue newMessage
-		EN_QUEUE(q_head, newMessage);
-	}
-	// print the number of messages that arrive
-	printf("\n[ Number of messages that arrive ]: %d\n", n);
+	printf("# SEVER_FREE()-> Number of messages that are sent successfully : %d\n", sentNum);
 }
 
 void STATICS(int totalArrived, int totalMessageSent, int totalRunTime,\
-			 int totalQueue, int firstTimes, int secondTimes){
+			 int totalQueue, int firstTimes, int secondTimes, int qMAX){
 	printf("\n\t===================================================\n");
 	printf("\t=                    [ 結果統計 ]                 =\n");
 	printf("\t===================================================\n");
-	// Total messages that arrived : totalArrived
 	printf(" \t* Total messages that arrived : %d\n", totalArrived);
-	// Total messages sent : totalMessageSent
 	printf(" \t* Total messages sent : %d\n", totalMessageSent);
-	// average number of messages successfully sent per minute: totalArrived/T
 	printf(" \t* Average number of successfully sent/minute: %.2lf\n",\
 			(double)totalArrived/totalRunTime);
-	// average number messages in the queue:  qTotalCount/T
 	printf(" \t* Average number messages in the queue: %.2lf\n",\
 	 		(double)totalQueue/totalRunTime);
-	// number of messages sent on the 1st attempt: 1stTimes
 	printf(" \t* Number of messages sent on the 1st attempt: %d\n", firstTimes);
-	// number of messages sent on the 2nd attempt: 2ndTimes
 	printf(" \t* Number of messages sent on the 2nd attempt: %d\n", secondTimes);
-	// average number of attempts to send a message successfully: 
-											// totalMessageSent/totalArrived
-	printf(" \t* Average number of attempts to send a message successfully: %.2lf\n", (double)totalMessageSent/totalArrived);
+	printf(" \t* Average number of attempts to send a message successfully: %.2lf\n",\
+			(double)totalMessageSent/totalArrived);
 	printf(" \t* Total run: %d\n", totalRunTime);
+	printf(" \t* Max size of Queue: %d\n", qMAX);
 }
 
 void EN_QUEUE(Q_HEAD *q, MESSAGES *element){
-	if(!q)
-		{ printf("\t[ warning: 101 ]\n"); return ; }
+	if(!q OR !element) { printf("\t[ warning: 101 ]\n"); return ; }
 	NODE *newnode = (NODE*)malloc(sizeof(NODE));
 	newnode->val = element;
 	if(q->next){ 
@@ -190,6 +197,7 @@ void EN_QUEUE(Q_HEAD *q, MESSAGES *element){
 	q->count += 1;
 }
 MESSAGES* DE_QUEUE(Q_HEAD *q){
+	if(!q OR !q->next) { printf("\t[ warning: 102 ]\n"); return NULL; }
 	NODE *tmp = q->next;
 	MESSAGES *ret = tmp->val;
 	q->next = q->next->next;
@@ -200,4 +208,29 @@ MESSAGES* DE_QUEUE(Q_HEAD *q){
 		return NULL;
 	}
 	return ret;
+}
+
+void CONDITION_SET(int* run, int*newMessageNum, int*processorRate, float *failedRate){
+	char chose='1';
+	printf("\n\t@郵件寄送系統模擬@\n");
+	printf("是否要使用預設值(1:是, 任意鍵:否): "); scanf("%c", &chose); fflush(stdin);
+	if(chose IS '1'){
+		run[0] = 100;
+		newMessageNum[0] = 0; newMessageNum[1] = 60;
+		processorRate[0] = 20; processorRate[1] = 60;
+		failedRate[0] = 0.3;
+		return;
+	}
+	printf("\t#請依序輸入模擬條件#\n");
+	printf("Iteration times: "); scanf("%d", run);
+	printf("Range of newMessageNum(start end): "); scanf("%d %d", &newMessageNum[0], &newMessageNum[1]);
+	printf("Range of processorRate(start end: "); scanf("%d %d", &processorRate[0], &processorRate[1]);
+	printf("Failed Rate(0~1.0): "); scanf("%f", failedRate);
+}
+void CONDITION_PRINT(int run, int *newMessageNum, int *processorRate, float failedRate){
+	printf("\t---\n\tPS. Condition of this test:\n");
+	printf("\t \trun set: %d\n", run);
+	printf("\t \tnewMessageNum: %d ~ %d\n", newMessageNum[0], newMessageNum[1]);
+	printf("\t \tprocessorRate: %d ~ %d\n", processorRate[0], processorRate[1]);
+	printf("\t \tfailedRate: %d %%\n\n", (int)(failedRate*100));
 }
