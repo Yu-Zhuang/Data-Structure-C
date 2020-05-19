6e09f6de79c3f2a1b1c54ee27aa14d40cd9e4480
mild-guy <106711129@gms.tcu.edu.tw>
2020-05-19 16:35:30 +0800

    update

--- 15_1_tree_expressionTree.c
+++ 15_1_tree_expressionTree.c
@@ -38,34 +38,8 @@ NODE* NODE_CREATE(char element){
 	return newnode;	
 }
 
-NODE* EXPRESSION_TREE_CREATE(char *prefix){
-	// stack : store child of tree node
-	STACK *stack = (STACK*)malloc(sizeof(STACK));
-	stack->next = NULL;
-	// tree root for return
-	NODE *ret = NODE_CREATE(prefix[0]);
-	PUSH(stack, &ret->right); 
-	PUSH(stack, &ret->left);
-	// go through prefix
-	int str_len = strlen(prefix);
-	for(int i=1; i<str_len; i++){
-		// create newnode
-		NODE *newnode = NODE_CREATE(prefix[i]); 
-		// if element is operand
-		if( ((prefix[i]>='A')AND(prefix[i]<='Z')) OR ((prefix[i]>='a')AND(prefix[i]<='z')) )
-			POP(stack)[0] = newnode;
-		// if element is operator
-		else if((prefix[i] IS '+') OR (prefix[i] IS '-') OR (prefix[i] IS '*') OR (prefix[i] IS '/')){
-			POP(stack)[0] = newnode;	
-			PUSH(stack, &newnode->right);
-			PUSH(stack, &newnode->left);		
-		}
-	}
-	return ret;
-}
-
 bool PARENTHESES_CHECK_NEED(char pre, NODE *current, NODE *preNode){
-	if( (pre IS '/' AND current->val IS '+') OR (pre IS '/' AND current->val IS '+')\
+	if( (pre IS '/' AND current->val IS '+') OR (pre IS '/' AND current->val IS '-')\
 		OR (pre IS '*' AND current->val IS '+') OR (pre IS '*' AND current->val IS '-')\
 		OR (pre IS '/' AND current->val IS '/' AND preNode->right IS current)\
 		OR (pre IS '/' AND current->val IS '*' AND preNode->right IS current)\
@@ -81,6 +55,7 @@ void EXPRESSION_TREE_PRINT(NODE *root, char pre, NODE *preNode){
 		if( ((root->val>='A')AND(root->val<='Z')) OR ((root->val>='a')AND(root->val<='z')) )
 			printf("%c ", root->val);
 		else{
+		// if element is operator
 			if( PARENTHESES_CHECK_NEED(pre, root, preNode) )
 				printf("( ");
 			EXPRESSION_TREE_PRINT(root->left, root->val, root);
@@ -92,19 +67,45 @@ void EXPRESSION_TREE_PRINT(NODE *root, char pre, NODE *preNode){
 	}
 }
 
-void GET_EXPRESSION(char *take){
-	FILE *fptr = fopen("./1.cpp","r");
-	fgets(take, MAX, fptr);
-	fclose(fptr);
+NODE* EXPRESSION_TREE_CREATE(char *prefix){
+	// stack : store child of tree node
+	STACK *stack = (STACK*)malloc(sizeof(STACK));
+	stack->next = NULL;
+	// tree root for return
+	NODE *ret = NODE_CREATE(prefix[0]);
+	PUSH(stack, &ret->right); 
+	PUSH(stack, &ret->left);
+	printf(" Current tree(inorder): ");
+	EXPRESSION_TREE_PRINT(ret, '\0', NULL);
+	printf("\n");
+	// go through prefix
+	int str_len = strlen(prefix);
+	for(int i=1; i<str_len; i++){
+		// create newnode
+		NODE *newnode = NODE_CREATE(prefix[i]); 
+		// if element is operand
+		if( ((prefix[i]>='A')AND(prefix[i]<='Z')) OR ((prefix[i]>='a')AND(prefix[i]<='z')) )
+			POP(stack)[0] = newnode;
+		// if element is operator
+		else if((prefix[i] IS '+') OR (prefix[i] IS '-') OR (prefix[i] IS '*') OR (prefix[i] IS '/')){
+			POP(stack)[0] = newnode;	
+			PUSH(stack, &newnode->right);
+			PUSH(stack, &newnode->left);	
+		}
+		printf(" Current tree(inorder): ");
+		EXPRESSION_TREE_PRINT(ret, '\0', NULL);
+		printf("\n");
+	}
+	return ret;
 }
 
 int main(void){
 	// read a prefix expression
 	char prefix[MAX] = "+-abc";
-	GET_EXPRESSION(prefix);
 	// create expression tree
 	NODE *root = EXPRESSION_TREE_CREATE(prefix);
 	// print tree
+	printf("\n #Result: ");
 	EXPRESSION_TREE_PRINT(root, '\0', NULL);
 	// end
 	printf("\n");
