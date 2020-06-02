4e06c7aea958afe2c47f2a10d3a9672dcb864087
mild-guy <106711129@gms.tcu.edu.tw>
2020-06-02 11:20:22 +0800

    update

--- 17_1_Sorting.cpp
+++ 17_1_Sorting.cpp
@@ -3,8 +3,17 @@
 # include <time.h>
 
 void GET_CHOSE(int *chose){
-    printf("\n\t@ Sorting @\n(1) Quick sort (rand 1 pivot)\n(2) Quick sort (check 5 item to get pivot)\n(3) Merge sort\n(4) Bubble sort\nEnter chose: ");
+    printf("\n\t@ Sorting @\n(1) Quick sort (rand 1 pivot)\n(2) Quick sort (check 5 item to get pivot)\n(3) Merge sort\n(4) Bubble sort\n(5) Compare each method\nEnter chose: ");
     scanf("%d", chose);
+}
+
+void ARRAY_PRINT(int *nums, int numsSize){
+    printf("\n\t\t========= SORTED_ARRAY_PRINT ===========\n");
+    for(int i=0;i<numsSize;i++){
+        printf("%7d ", nums[i]);
+        if((i+1)%10 == 0)
+            printf("\n");
+    }
 }
 
 void RESULT_PRINT(int *arrayA, int arraySize, int cTimes, int max, int min, int chose){
@@ -29,15 +38,6 @@ int* ARRAY_GENERATOR(int max, int min, int numsSize){
     for(int i=0; i<numsSize; i++)
         ret[i] = rand()%(max-min+1)+min;
     return ret;
-}
-
-void ARRAY_PRINT(int *nums, int numsSize){
-    printf("\n\t\t========= SORTED_ARRAY_PRINT ===========\n");
-    for(int i=0;i<numsSize;i++){
-        printf("%7d ", nums[i]);
-        if((i+1)%10 == 0)
-            printf("\n");
-    }
 }
 
 void BUBBLE_SORT(int *nums, int left, int right, int *cp){
@@ -85,19 +85,29 @@ void MERGE(int *nums, int s1, int e1, int s2, int e2, int *ct){
     int len_1 = e1 - s1 + 1, len_2 = e2 - s2 + 1;
     int i=0, j=0, n=0;
     while(i<len_1 || j<len_2){
-        if( (i<len_1 && j<len_2 && nums[s1+i] < nums[s2+j]) || (i<len_1&&j==len_2)){
+        if(j == len_2){
+            ret[n] = nums[s1+i];
+            i++;            
+        }
+        else if(i == len_1){
+            ret[n] = nums[s2+j];
+            j++; 
+        }
+        else if(nums[s1+i] < nums[s2+j]){
             ret[n] = nums[s1+i];
             i++;
+            ct[0]++;
         }
-        else{
+        else if(nums[s1+i] >= nums[s2+j]){
             ret[n] = nums[s2+j];
-            j++;            
+            j++;   
+            ct[0]++;         
         }
         n++;             
     }
     for(int m=0;m<e2-s1+1; m++)
         nums[s1+m] = ret[m];
-    ct[0] += n;
+    
     free(ret);
 }
 
@@ -107,6 +117,37 @@ void MERGE_SORT(int *nums, int start, int end, int *ct){
     MERGE_SORT(nums, start, (start+end)/2, ct);
     MERGE_SORT(nums, (start+end)/2+1, end, ct);
     MERGE(nums, start, (start+end)/2, (start+end)/2+1, end, ct);
+}
+
+void COMPARE_RUN(int max, int min, int arraySize){
+    int QS_1=0, QS_2=0, MS=0, BS=0;
+    double QS_1_R=0, QS_2_R=0, MS_R=0, BS_R=0;
+    int round = 0;
+    printf("how many round: ");
+    scanf("%d", &round);
+    for(int i=0;i<round;i++){
+        int *arrayA = ARRAY_GENERATOR(max, min, arraySize);
+        QUICK_SORT(arrayA, 0, arraySize-1, &QS_1, 1);
+        free(arrayA);
+    }
+    for(int i=0;i<round;i++){
+        int *arrayA = ARRAY_GENERATOR(max, min, arraySize);
+        QUICK_SORT(arrayA, 0, arraySize-1, &QS_2, 2);
+        free(arrayA);
+    }
+    for(int i=0;i<round;i++){
+        int *arrayA = ARRAY_GENERATOR(max, min, arraySize);
+        MERGE_SORT(arrayA, 0, arraySize-1, &MS);
+        free(arrayA);
+    }
+    for(int i=0;i<round;i++){
+        int *arrayA = ARRAY_GENERATOR(max, min, arraySize);
+        BUBBLE_SORT(arrayA, 0, arraySize-1, &BS);
+        free(arrayA);
+    }
+    QS_1_R=(double)QS_1/round; QS_2_R=(double)QS_2/round; MS_R=(double)MS/round; BS_R=(double)BS/round;
+    printf("\n\t# Condition:\n\t\t* array size: %d\n\t\t* number range: %d ~ %d\n\t\t* sorting method: compare each sorting method\n\t\t* round: %d", arraySize, min, max, round);
+    printf("\n\t# Result (average comparison times):\n\t\t* Quick sort (rand 1 pivot): %lf\n\t\t* Quick sort (check 5 item to get pivot): %lf\n\t\t* Merge sort: %lf\n\t\t* Bubble sort: %lf", QS_1_R, QS_2_R, MS_R, BS_R);
 }
 
 int main(void){  
@@ -121,6 +162,7 @@ int main(void){
             case 1: case 2: QUICK_SORT(arrayA, 0, arraySize-1, &cTimes, chose); break;
             case 3: MERGE_SORT(arrayA, 0, arraySize-1, &cTimes); break;
             case 4: BUBBLE_SORT(arrayA, 0, arraySize-1, &cTimes); break;
+            case 5: COMPARE_RUN(max, min, arraySize); break;
             default: printf("\t[ warning : wrong input ]\n"); break;
         }
         if(chose>0 && chose<5){
